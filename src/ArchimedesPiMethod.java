977fed14a3011039919de972c508053704deb131
Justin hart <justinhart201@outlook.com>
2023-12-05 08:06:37 -0800

    assn2

--- src/ArchimedesPiMethod.java
+++ src/ArchimedesPiMethod.java
@@ -1,7 +1,28 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 public class ArchimedesPiMethod {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of sides for the polygon:");
+        int n = scanner.nextInt();
 
+        // Step 2: Calculate the angle B
+        double B = 360.0 / n;
+
+        // Step 3: Calculate the angle A
+        double A = 0.5 * B;
+
+        // Step 4: Calculate the length of 1 triangle base s
+        double s = 2 * Math.sin(Math.toRadians(A));
+
+        // Step 5: Get the polygon perimeter p
+        double p = n * s;
+
+        // Step 6: Estimate PI
+        double PI = p / 2;
+
+        System.out.println("Estimated value of PI is: " + PI);
     }
 }
+
